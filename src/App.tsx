74bec19a74c2d57f0dfe2098da36eb43379dcce3
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-26 23:49:37 +0600

    added animation in div and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <div className="main">
-      <div className="box-1"></div>
+      {/* <div className="box-1"></div>
+      <div className="box-container-2">
+        <div className="box-2"></div>
+      </div> */}
+      <button className="btn">Hover me</button>
     </div>
   );
 }
