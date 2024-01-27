a636b25f3e3ef1dece04f0768824ec2fb4ab2712
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-27 16:39:46 +0600

    added window shape animation

--- src/pages/Lesson1.tsx
+++ src/pages/Lesson1.tsx
@@ -1,15 +1,46 @@
 import { motion } from "framer-motion";
 const initial = { rotate: 0 };
 const animate = { rotate: 360 };
+const parent = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
+const child = {
+  hidden: { opacity: 0, scale: 0.1 },
+  visible: { opacity: 1, scale: 1 },
+};
 const Lesson1 = () => {
   return (
     <div>
       <motion.div
-        className="size-64 bg-indigo-500 rounded-lg"
-        initial={initial}
-        animate={animate}
-        transition={{ type: "spring", duration: 5 }}
-      ></motion.div>
+        className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+        variants={parent}
+        initial="hidden"
+        animate="visible"
+        transition={{
+          ease: "easeInOut",
+          duration: 2,
+          delayChildren: 0.5,
+          staggerChildren: 0.5,
+        }}
+      >
+        <motion.div
+          className="size-20 bg-cyan-400 rounded-sm"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-cyan-400 rounded-sm"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-cyan-400 rounded-sm"
+          variants={child}
+        ></motion.div>
+        <motion.div
+          className="size-20 bg-cyan-400 rounded-sm"
+          variants={child}
+        ></motion.div>
+      </motion.div>
     </div>
   );
 };
