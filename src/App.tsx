478d855d3b1465f9506aebda1bfa15f6776e3f09
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-27 08:09:45 +0600

    added loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 function App() {
   return (
     <div className="main">
-      {/* <div className="box-1"></div>
-      <div className="box-container-2">
+      <div className="loading">
+        <div className="glass"></div>
+
+        <div className="box"></div>
+      </div>
+      {/* <div className="box-container-2">
         <div className="box-2"></div>
       </div> */}
-      <button className="btn">Hover me</button>
+      {/* <button className="btn">Hover me</button> */}
     </div>
   );
 }
